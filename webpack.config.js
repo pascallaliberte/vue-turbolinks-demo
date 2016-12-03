6df4dad48c75c9801a0ae05b710854be2c3a8b34
Pascal Laliberté <pascal@pascallaliberte.me>
2016-12-02 23:14:25 -0500

    Configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: './_assets/app.js',
+  output: {
+    path: './assets',
+    filename: 'app.js'
+  },
+  module: {
+    // Special compilation rules
+    loaders: [
+      {
+        // Ask webpack to check: If this file ends with .js, then apply some transforms
+        test: /\.js$/,
+        // Transform it with babel
+        loader: 'babel',
+        // don't transform node_modules folder (which don't need to be compiled)
+        exclude: /node_modules/
+      }
+    ]
+  }
+}
