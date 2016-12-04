844c67dcfa563169a50fe401ef16c70516401995
Pascal Laliberté <pascal@pascallaliberte.me>
2016-12-04 16:41:00 -0500

    vue-turbolinks: Use vnode.tag instead of uid, restore on beforeUpdate

--- _assets/vue/mixins/vue-turbolinks.js
+++ _assets/vue/mixins/vue-turbolinks.js
@@ -2,20 +2,28 @@ import Vue from 'vue'
 
 export default {
   beforeMount() {
-    if (this === this.$root) {
-      this._turbolinksOriginalEl = this.$el.outerHTML
-    }
-  },
-  created() {
-    if (!this.$root.hasOwnProperty('_turbolinksState') || undefined === this.$root._turbolinksState[this._uid]) {
+    if (this !== this.$root) {
       return
     }
-    let data = this.$root._turbolinksState[this._uid]
+    this._turbolinksOriginalEl = this.$el.outerHTML
+    let state = this.$el.getAttribute('turbolinks-state')
+
+    if (state) {
+      this._turbolinksState = JSON.parse(state)
+    }
+  },
+  beforeUpdate() {
+    let id = this._turbolinksGetUniqueLabel(this)
+    if (!this.$root.hasOwnProperty('_turbolinksState') || undefined === this.$root._turbolinksState[id]) {
+      return
+    }
+    let data = this.$root._turbolinksState[id]
     for (let key of Object.keys(data)) {
       if (this.hasOwnProperty(key)) {
         this[key] = data[key]
       }
     }
+    delete this.$root._turbolinksState[id]
   },
   beforeDestroy() {
     if (this !== this.$root) {
@@ -30,10 +38,16 @@ export default {
       for (let child of currentInstance.$children) {
         instancesToParse.push(child)
       }
-      root._turbolinksState[currentInstance._uid] = JSON.parse(JSON.stringify(currentInstance.$data))
+      let id = this._turbolinksGetUniqueLabel(currentInstance)
+      root._turbolinksState[id] = JSON.parse(JSON.stringify(currentInstance.$data))
     }
 
     this.$el.outerHTML = this._turbolinksOriginalEl
     document.querySelector(this.$options.el).setAttribute('turbolinks-state', JSON.stringify(this._turbolinksState))
+  },
+  methods: {
+    _turbolinksGetUniqueLabel(vm) {
+      return (vm === vm.$root)? 'root': vm.$vnode.tag
+    }
   }
 }
