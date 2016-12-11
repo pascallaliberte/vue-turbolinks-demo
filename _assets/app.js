bbef2150559cb7d6e2eefd263d72879c0444b083
Pascal Laliberté <pascal@pascallaliberte.me>
2016-12-11 17:48:06 -0500

    Remove console.log calls

--- _assets/app.js
+++ _assets/app.js
@@ -17,12 +17,10 @@ function initPostPreview() {
   window.isInitPostPreview = true
 
   let loadPostPreview = function(e) {
-    console.log(e.type)
     if (!document.querySelector(postPreviewEl)) {
       return
     }
     if (e.type == 'pageshow' && window.postPreview && !window.postPreview._isDestroyed) {
-      console.log(window.postPreview)
       return
     }
 
@@ -46,7 +44,6 @@ function initPostPreview() {
     }
     if (window.postPreview) {
       window.postPreview.$destroy()
-      console.log("postPreview._isDestroyed: " + window.postPreview._isDestroyed)
     }
   })
 }
--- assets/app.js
+++ assets/app.js
@@ -74,12 +74,10 @@
 	  window.isInitPostPreview = true;
 
 	  var loadPostPreview = function loadPostPreview(e) {
-	    console.log(e.type);
 	    if (!document.querySelector(postPreviewEl)) {
 	      return;
 	    }
 	    if (e.type == 'pageshow' && window.postPreview && !window.postPreview._isDestroyed) {
-	      console.log(window.postPreview);
 	      return;
 	    }
 
@@ -103,7 +101,6 @@
 	    }
 	    if (window.postPreview) {
 	      window.postPreview.$destroy();
-	      console.log("postPreview._isDestroyed: " + window.postPreview._isDestroyed);
 	    }
 	  });
 	}
